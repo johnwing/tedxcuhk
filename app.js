63cea8a4d2a0f9d4aa09949d0d3344e11d9507de
johnwing <47427803+johnwing@users.noreply.github.com>
2020-02-03 00:37:08 +0800

    uploadExpresss

--- app.js
+++ app.js
@@ -2,10 +2,25 @@
 const express = require('express');
 const app = express(); //建立一個Express伺服器
 
-app.listen(3000, function () {
+// check running enviroment
+var port = process.env.PORT || 3000;
+
+
+app.listen(port, function () {
   console.log('Example app is running on port 3000!');}
 );
 
 app.get('/', function (req, res) {
-  res.send('Express is excellent!')
+  res.send('Express is excellent!2')
+});
+
+app.get('/about', function(req, res) {
+    res.send('Send about page!')
+});
+app.get('/about/about-me', function(req, res) {
+    res.send('Send about-me page!')
+});
+
+app.get('/about/:name/:nickname', function(req, res) {
+  res.send(req.params.name + ' is so ' + req.params.nickname);
 });
