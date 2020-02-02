7b84bf26fc79a403a67608c809dc79f699c23bc7
johnwing <47427803+johnwing@users.noreply.github.com>
2020-02-02 21:31:54 +0800

    update

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 
 const express = require('express');
 const app = express(); //建立一個Express伺服器
-app.listen(3000); //告訴server聽取3000這個Port
-
 
 app.listen(3000, function () {
   console.log('Example app is running on port 3000!');}
